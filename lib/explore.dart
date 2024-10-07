819ce6c3ca612c6f88a67d9686131aa43cb7c098
Jakob Neufeld <130662630+dzoidbrg@users.noreply.github.com>
2024-10-07 23:15:45 +0200

    Sigma

---
+++ lib/explore.dart
@@ -0,0 +1,26 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as models;
+class ExplorePage extends StatefulWidget {
+  const ExplorePage({super.key});
+
+  @override
+  State<ExplorePage> createState() => _ExplorePageState();
+}
+
+class _ExplorePageState extends State<ExplorePage> {
+  late models.User user;
+  
+  @override
+  void initState() {
+      // TODO: implement initState
+    
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return List();
+  }
+}
